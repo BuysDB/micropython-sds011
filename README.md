b9d456d959535c3b3d0feba8c94ac1e5ba02d8c8
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2018-06-25 15:21:27 +0200

    Switch to on-demand measurement type, UART initialization outsize of sensor class, change repo structure and documentation to match Adafruit-style CircuitPython modules.

--- README.md
+++ README.md
@@ -1,60 +1,43 @@
-# Pollution monitor
+Introduction
+============
 
-Components:
+A MicroPython driver for the SDS011 particle sensor (PM10 and PM25). This sensor
+uses serial communication.
 
-- SDS011 particulate sensor
-- Wemos D1 mini pro (based on ESP8266)
-- Some wires
+Installation
+=============
 
-Ultra simple wiring and ultra simple programming with Micropython! The SDS011 communicates over serial with a protocol detailed here http://cl.ly/ekot: 
+On a LoPy, just put ``sds011.py`` in the ``lib/`` directory.
 
-SDS011 -- Wemos 
+Usage Notes
+=============
 
-  TX   --   RX    
-  RX   --   D4 (TX1)  
-  5v   --   5v  
-  GND  --   GND   
+First, import the library:
 
-Also, connect Wemos RST -- Wemos D0 (used for interrupting deep sleep).
+.. code-block:: python
 
-Wemos has only one serial-in and Micropython has no software serial library, so the device cannot be accessed when it is connected to the sensor. Luckily, the webrepl can be used to interact with the device instead.
+    import sds011
 
-The sensor RX is connected to D4 because of a problem when connected to TX on the Wemos:
- - Printing anything to webrepl wakes the sensor up and it ceases to interpret sleep and wake signals properly
- - Sending the Wemos into deep sleep also wakes up the sensor!
+Next, initialize a UART object (here P21 in TX and P22 is RX):
 
-D4 is a separate serial-out pin which does not suffer from these problems, probably caused by interference with the webrepl.
+.. code-block:: python
 
-As currently programmed, the sensor will loop between sleeping (with the Wemos in deep sleep) and reading. It is also possible to set the dutycycle mode on the SDS011 (where the sensor automatically sleeps for a specified interval), but there is not much benefit to this over sleeping (especially as in this mode it takes 60 secs to wake up before reading, whereas wake up from sleep takes 30 secs)
+	uart = UART(1, baudrate=9600, pins=('P21','P22'))
 
-## Going outdoors
 
-A downside of the current implementation is that the SDS011 draws a relatively hefty 4ma while asleep. Some possible solutions for increasing battery life:
-1) Use something (relay? MOSFET?) between the Wemos and the SDS011 to cut the connection before entering deep sleep. 
-2) Hook the whole circuit to a MOSFET controlled by an ATiny85 or similar. This would provide better efficiency (because we would be sleeping the ATiny85 instead of the Wemos, which would be completely powered down), but is more complicated. 
+Since we have the UART bus object, we can now use it to instantiate the SDS011 object:
 
-Let's evaluate these options assuming a 10 minute wake interval, 30 sec working period (so 3 minutes / hour awake), and the following power factors:
- - SDS011 consumes 70mA (awake) or <4mA (asleep)
- - Wemos consumes 150mA on average when awake or 5mA in light sleep
- - Deepsleep on Wemos consumes 2/3 times more than on esp8266 because of the USB-TTL converter: probably equates to around 0.1mA in total 
- - At 1MHz the ATTiny85 consumes 2.5mA (awake) or 0.0005mA (asleep)
- - 10000mAh usb battery pack (e.g. [this one](https://www.amazon.co.uk/Puridea-Portable-External-Blackberry-Valentines/dp/B0109PYRE0/ref=sr_1_4?s=electronics&ie=UTF8&qid=1499622375&sr=1-4&keywords=10000+mah+usb+power))
- - 70% efficiency 
+.. code-block:: python
 
-As is: (10000 * 60 * 0.7) / (3 * (70 + 150) + 57 * (0.1 + 4) = 470 hours or 20 days 
+	dust_sensor = sds011.SDS011(uart)
 
-Without ATTiny but with relay: (10000 * 60 * 0.7) / (3 * (70 + 150) + 57 * 0.1) = 631 hours or 26 days 
+Reading from the Sensor
+------------------------
 
-With ATTiny: (10000 * 60 * 0.7) / (3 * (70 + 150 + 2.5) + 57 * 0.0005) = 629 hours 
+To read from the sensor:
 
-What about using the ATTiny and turning on the Wemos only for last 7 secs of 30sec working period (say, 0.7 mins / hour @ 200mA)? (10000 * 60 * 0.7) / (3 * (70 + 2.5) + 0.7 * 200 + 57 * 0.0005) = 1174 hours or 49 days.
+.. code-block:: python
 
-If the Wemos can request time and pass it to ATTiny85, we can sleep 6 hours at night as well, bringing us up to around 60 days. 
-
-## Portability
-
-Another desideratum is the ability pair with a phone and transmit on the fly, or store measurements for transmission later.
-
-Storing: by default we get 6 bytes of data every second. After 30 mins that's 11kb. The Wemos has 96 kb of data RAM so we should be fine... We could also move into the 4 mb of flash, but must be careful as it is limited to 100,000 write/erase cycles.
-
-For gps data we need the phone. It should be possible for the phone to connect to the Wemos set up as an access point. Battery life on wifi might be expected to be 10000 * 0.7 / 200 = 35 hours. But it might make more sense to move to the ESP32 (which also has bluetooth) for this. 
+    dust_sensor.read()
+	print('PM25: ', dust_sensor.pm25)
+	print('PM10: ', dust_sensor.pm10)
--- sds011.py
+++ sds011.py
@@ -14,87 +14,106 @@ Reading format. See http://cl.ly/ekot
 
 """
 
-import machine
 import ustruct as struct
-import mqtt
 import sys
 import utime as time
 
-def init_uart(x):
-    uart = machine.UART(x, 9600)
-    uart.init(9600, bits=8, parity=None, stop=1)
-    return uart
-
-CMDS = {'SET': b'\x01',
+_SDS011_CMDS = {'SET': b'\x01',
         'GET': b'\x00',
+        'QUERY': b'\x04',
+        'REPORTING_MODE': b'\x02',
         'DUTYCYCLE': b'\x08',
         'SLEEPWAKE': b'\x06'}
 
-def make_command(cmd, mode, param):
-    header = b'\xaa\xb4'
-    padding = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff'
-    checksum = chr(( ord(cmd) + ord(mode) + ord(param) + 255 + 255) % 256)
-    tail = b'\xab'
-    return header + cmd + mode + param + padding + bytes(checksum, 'utf8') + tail
+class SDS011:
+    def __init__(self, uart):
+        self._uart = uart
+        self._pm25 = 0.0
+        self._pm10 = 0.0
+        self._packet_status = ''
+        self._packet = ()
 
-# sensor wakes for 60 secs before issuing measurement
-def set_dutycycle(rest_mins):
-    uart = init_uart(1)
-    cmd = make_command(CMDS['DUTYCYCLE'], CMDS['SET'], chr(rest_mins))
-    print('Setting sds011 to read every', rest_mins, 'minutes:', cmd)
-    uart.write(cmd)
+        self.set_reporting_mode_query()
 
-def wake():
-    uart = init_uart(1)
-    cmd = make_command(CMDS['SLEEPWAKE'], CMDS['SET'], chr(1))
-    print('Sending wake command to sds011:', cmd)
-    uart.write(cmd)
+    @property
+    def pm25(self):
+        return self._pm25
 
-def sleep():
-    uart = init_uart(1)
-    cmd = make_command(CMDS['SLEEPWAKE'], CMDS['SET'], chr(0))
-    print('Sending sleep command to sds011:', cmd)
-    uart.write(cmd)
+    @property
+    def pm10(self):
+        return self._pm10
 
-def process_reply(packet):
-    print('Reply received:', packet)
+    @property
+    def packet_status(self):
+        return self._packet_status
 
-def process_measurement(packet):
-    try:
-        print('\nPacket:', packet)
-        *data, checksum, tail = struct.unpack('<HHBBBs', packet)
-        pm25 = data[0]/10.0
-        pm10 = data[1]/10.0
-        # device_id = str(data[2]) + str(data[3])
-        checksum_OK = checksum == (sum(data) % 256)
-        tail_OK = tail == b'\xab'
-        packet_status = 'OK' if (checksum_OK and tail_OK) else 'NOK'
-        print('PM 2.5:', pm25, '\nPM 10:', pm10, '\nStatus:', packet_status)
-        mqtt.publish({
-            'pm25': pm25,
-            'pm10': pm10,
-            'status': packet_status
-        })
-    except Exception as e:
-        print('Problem decoding packet:', e)
-        sys.print_exception(e)
+    @property
+    def packet(self):
+        return self._packet
 
-def read(allowed_time=0):
-    uart = init_uart(0)
-    start_time = time.ticks_ms()
-    delta_time = 0
-    while (delta_time <= allowed_time * 1000):
+    def make_command(self, cmd, mode, param):
+        header = b'\xaa\xb4'
+        padding = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff'
+        checksum = chr(( ord(cmd) + ord(mode) + ord(param) + 255 + 255) % 256)
+        checksum = bytes(checksum, 'utf8')
+        tail = b'\xab'
+
+        return header + cmd + mode + param + padding + checksum + tail
+
+    #Sends wake command to sds011
+    def wake(self):
+        cmd = self.make_command(_SDS011_CMDS['SLEEPWAKE'],
+                _SDS011_CMDS['SET'], chr(1))
+        self._uart.write(cmd)
+
+    #Sends sleep command to sds011
+    def sleep(self):
+        cmd = self.make_command(_SDS011_CMDS['SLEEPWAKE'],
+                _SDS011_CMDS['SET'], chr(0))
+        self._uart.write(cmd)
+
+    def set_reporting_mode_query(self):
+        cmd = self.make_command(_SDS011_CMDS['REPORTING_MODE'],
+                _SDS011_CMDS['SET'], chr(1))
+        self._uart.write(cmd)
+
+    #Query new measurement data
+    def query(self):
+        cmd = self.make_command(_SDS011_CMDS['QUERY'], chr(0), chr(0))
+        self._uart.write(cmd)
+
+    def process_measurement(self, packet):
         try:
-            header = uart.read(1)
-            if header == b'\xaa':
-                command = uart.read(1)
-                if command == b'\xc0':
-                    packet = uart.read(8)
-                    process_measurement(packet)
-                elif command == b'\xc5':
-                    packet = uart.read(8)
-                    process_reply(packet)
-            delta_time = time.ticks_diff(time.ticks_ms(), start_time) if allowed_time else 0
+            *data, checksum, tail = struct.unpack('<HHBBBs', packet)
+            self._pm25 = data[0]/10.0
+            self._pm10 = data[1]/10.0
+            checksum_OK = (checksum == (sum(data) % 256))
+            tail_OK = tail == b'\xab'
+            self._packet_status = 'OK' if (checksum_OK and tail_OK) else 'NOK'
         except Exception as e:
-            print('Problem attempting to read:', e)
+            print('Problem decoding packet:', e)
             sys.print_exception(e)
+
+    def read(self):
+        #Query measurement
+        self.query()
+
+        #Read measurement
+        #Drops up to 512 bits before giving up finding a measurement pkt...
+        for i in range(512):
+            try:
+                header = self._uart.read(1)
+                if header == b'\xaa':
+                    command = self._uart.read(1)
+
+                    if command == b'\xc0':
+                        packet = self._uart.read(8)
+                        if packet != None:
+                            self.process_measurement(packet)
+                            return 'OK'
+            except Exception as e:
+                print('Problem attempting to read:', e)
+                sys.print_exception(e)
+
+        #If we gave up finding a measurement pkt
+        return 'NOK'
