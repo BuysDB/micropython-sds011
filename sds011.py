ad9d22694e21a40585f43d6a56b2867ef048cde1
Alexandre Marquet <alexandre.marquet@imt-atlantique.fr>
2018-08-01 19:00:36 +0200

    Improved documentation.

--- sds011.py
+++ sds011.py
@@ -16,7 +16,6 @@ Reading format. See http://cl.ly/ekot
 
 import ustruct as struct
 import sys
-import utime as time
 
 _SDS011_CMDS = {'SET': b'\x01',
         'GET': b'\x00',
@@ -26,29 +25,37 @@ _SDS011_CMDS = {'SET': b'\x01',
         'SLEEPWAKE': b'\x06'}
 
 class SDS011:
+    """A driver for the SDS011 particulate matter sensor.
+
+    :param uart: The `UART` object to use.
+    """
     def __init__(self, uart):
         self._uart = uart
         self._pm25 = 0.0
         self._pm10 = 0.0
-        self._packet_status = ''
+        self._packet_status = False
         self._packet = ()
 
         self.set_reporting_mode_query()
 
     @property
     def pm25(self):
+        """Return the PM2.5 concentration, in µg/m^3."""
         return self._pm25
 
     @property
     def pm10(self):
+        """Return the PM10 concentration, in µg/m^3."""
         return self._pm10
 
     @property
     def packet_status(self):
+        """Returns False if the received packet is corrupted."""
         return self._packet_status
 
     @property
     def packet(self):
+        """Return the last received packet."""
         return self._packet
 
     def make_command(self, cmd, mode, param):
@@ -60,14 +67,14 @@ class SDS011:
 
         return header + cmd + mode + param + padding + checksum + tail
 
-    #Sends wake command to sds011
     def wake(self):
+        """Sends wake command to sds011 (starts its fan)."""
         cmd = self.make_command(_SDS011_CMDS['SLEEPWAKE'],
                 _SDS011_CMDS['SET'], chr(1))
         self._uart.write(cmd)
 
-    #Sends sleep command to sds011
     def sleep(self):
+        """Sends sleep command to sds011 (stops its fan)."""
         cmd = self.make_command(_SDS011_CMDS['SLEEPWAKE'],
                 _SDS011_CMDS['SET'], chr(0))
         self._uart.write(cmd)
@@ -77,8 +84,8 @@ class SDS011:
                 _SDS011_CMDS['SET'], chr(1))
         self._uart.write(cmd)
 
-    #Query new measurement data
     def query(self):
+        """Query new measurement data"""
         cmd = self.make_command(_SDS011_CMDS['QUERY'], chr(0), chr(0))
         self._uart.write(cmd)
 
@@ -89,17 +96,23 @@ class SDS011:
             self._pm10 = data[1]/10.0
             checksum_OK = (checksum == (sum(data) % 256))
             tail_OK = tail == b'\xab'
-            self._packet_status = 'OK' if (checksum_OK and tail_OK) else 'NOK'
+            self._packet_status = True if (checksum_OK and tail_OK) else False
         except Exception as e:
             print('Problem decoding packet:', e)
             sys.print_exception(e)
 
     def read(self):
+        """
+        Query a new measurement, wait for response and process it.
+        Waits for a response during 512 characters (0.4s at 9600bauds).
+        
+        Return True if a response has been received, False overwise.
+        """
         #Query measurement
         self.query()
 
         #Read measurement
-        #Drops up to 512 bits before giving up finding a measurement pkt...
+        #Drops up to 512 characters before giving up finding a measurement pkt...
         for i in range(512):
             try:
                 header = self._uart.read(1)
@@ -110,10 +123,10 @@ class SDS011:
                         packet = self._uart.read(8)
                         if packet != None:
                             self.process_measurement(packet)
-                            return 'OK'
+                            return True
             except Exception as e:
                 print('Problem attempting to read:', e)
                 sys.print_exception(e)
 
         #If we gave up finding a measurement pkt
-        return 'NOK'
+        return False
