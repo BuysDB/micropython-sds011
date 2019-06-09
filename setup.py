fb8786128954cb04371f24e317922708bfc0f60e
buysdb <i@buysdb.nl>
2019-06-09 22:37:16 +0200

    PIP compatible commits

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+	name='sds011',
+	version='0.01',
+	author='Sam Galson, Alexandre Marquet, Buys de Barbanson',
+	author_email='code@buysdb.nl',
+	description='',
+	url='https://github.com/BuysDB/micropython-sds011',
+	py_modules=['sds011'],
+	install_requires=[]
+)
